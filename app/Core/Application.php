44011768c044c75755822fa1d2ee395403ec7ad6
FedeM <mariottifederico@gmail.com>
2026-02-04 21:22:34 -0300

    docs: bump version to 1.9.3 and update roadmap

--- app/Core/Application.php
+++ app/Core/Application.php
@@ -9,7 +9,7 @@ class Application extends Container
      *
      * @var string
      */
-    const VERSION = '1.9.2';
+    const VERSION = '1.9.3';
 
     /**
      * The base path for the Phantom installation.
