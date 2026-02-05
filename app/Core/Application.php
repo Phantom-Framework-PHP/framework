289aa4ae12cc0677857323325c7d3a001316a3ff
FedeM <mariottifederico@gmail.com>
2026-02-04 22:15:22 -0300

    docs: bump version to 1.10.5 and update roadmap

--- app/Core/Application.php
+++ app/Core/Application.php
@@ -9,7 +9,7 @@ class Application extends Container
      *
      * @var string
      */
-    const VERSION = '1.10.4';
+    const VERSION = '1.10.5';
 
     /**
      * The base path for the Phantom installation.
